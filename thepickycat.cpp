cf31cc05cf9530a10dba8e1166ec49a4e2316caf
Syed Sami Sayem <syedsameislam10@gmail.com>
2025-08-03 20:15:03 +0600

    update

--- thepickycat.cpp
+++ thepickycat.cpp
@@ -6,60 +6,24 @@ using namespace std;
 void solve()
 {
    int n;cin>>n;
-   vector<int>arr(n+100);
+   vector<int>arr(n);
+   int median=((n+1)/2)-1;
 
-   for(int i=1;i<=n;i++)
+   for(int i=0;i<n;i++)
    {
       cin>>arr[i];
       arr[i]=abs(arr[i]);
    }
+   int f1=arr[0];
+
+   sort(arr.begin(),arr.end());
+   int cnt=0;
 
 
-   int mn=arr[1];
-   int ceiling=(n+1)/2;
-   int f=0;
-   sort(arr.begin()+1,arr.begin()+n+1);
+   for(int i=0;i<n;i++) if(arr[i]>f1) cnt++;
 
-   for(int i=1;i<=ceiling;i++)
-   {
-      if(arr[i]==mn)
-      {
-         f=1;
-         break;
-      }
-   }
-
-   // cout<<"position of :"<<mn<<endl<<index<<endl;
-
-   // for(auto it:arr) cout<<it<<" ";
-   //    cout<<endl;
-
-   
-   if(f==1 || (n%2==0&&arr[n/2+1]==mn))
-   {
-      cout<<"YES"<<endl;
-   }
-   else cout<<"NO"<<endl;
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // if(check<left) cout<<"YES"<<endl;
-   // else cout<<"NO"<<endl;
-
+      if(cnt>=median) cout<<"YES"<<endl;
+      else cout<<"NO"<<endl;
 }
  
 int32_t main()
