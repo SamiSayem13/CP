1f5955f7493f79d11be66a1790072eff7596d572
Syed Sami Sayem <syedsameislam10@gmail.com>
2025-08-08 00:38:03 +0600

    Add solution for submission bait problem
    Added submissionbait.cpp and its compiled binary, implementing a solution that checks for the presence of any element with an odd frequency in the input. Updated input and output files to reflect new test cases and results. Also added a skeleton for gorillaandpermutation.cpp.

---
+++ submissionbait.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+ 
+void solve()
+{
+   int n;cin>>n;
+   vector<int>arr(n+1);
+
+   for(int i=1;i<=n;i++)
+   {
+      int x;cin>>x;
+      arr[x]++;
+   }
+
+
+   for(int i=1;i<=n;i++)
+   {
+      if(arr[i]%2==1)
+      {
+         cout<<"YES"<<endl;
+         return;
+      }
+   }
+
+   cout<<"NO"<<endl;
+
+}
+ 
+int32_t main()
+{
+     ios_base::sync_with_stdio(0);
+     cin.tie(0);
+     int t;cin>>t;
+     while(t--)
+     {
+        solve();
+     }
+   
+     
+     return 0;
+}
